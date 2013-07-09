af74309b790ef45bb7b2001e9e2b319a4360d061
Ricardo Gomez <ricardo.gomez331@gmail.com>
2013-07-09 23:45:22 +0000

    it burns a little but this is all i have

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject wallman "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main wallman.core
+  :dependencies [[seesaw "1.4.3"]
+                 [org.clojure/clojure "1.4.0"]])
---
+++ src/wallman/core.clj
@@ -0,0 +1,32 @@
+(ns wallman.core
+  (:gen-class)
+  (:use seesaw.core)
+  (:import java.io.File))
+
+(def ^{:private true :doc "The user's home folder"} home (System/getProperty "user.home"))
+
+(defn- wallpapers [dir]
+  "Returns a seq of all wallpapers"
+  (when (instance? File dir)
+    (let [files (.listFiles dir)]
+      (zipmap (map #(.getName %) files) files))))
+
+(defn make-root-frame [dir]
+  (let [wallpapers (wallpapers dir)
+        lbox (listbox :model (keys wallpapers))
+        preview (label)
+        layout (border-panel :center lbox :east preview)
+        f (frame :content layout :title "Wallpaper Manager" :on-close :hide)]
+    (listen lbox :selection #(config! preview :icon (wallpapers (selection %))))
+    f))
+
+(defn- make-file [x & rst]
+  (loop [a (File. x) b rst]
+    (if (nil? (first b))
+      a
+      (recur (File. a (first b)) (rest b)))))
+
+(defn -main [& args]
+  (-> (make-root-frame (make-file (first args)))
+      pack!
+      show!))
