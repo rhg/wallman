148e320e864a91bfb3c6d337c39e816e6d63568b
Ricardo Gomez <ricardo.gomez331@gmail.com>
2013-07-11 20:41:24 +0000

    Works for local folders

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject wallman "0.1.0-SNAPSHOT"
+(defproject wallman "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :aot [wallman.core]
   :main wallman.core
   :dependencies [[seesaw "1.4.3"]
+                 [hu.kazocsaba/image-viewer "1.2.3"]
                  [org.clojure/clojure "1.4.0"]])
--- src/wallman/core.clj
+++ src/wallman/core.clj
@@ -1,32 +1,49 @@
-(ns wallman.core
-  (:gen-class)
+(ns wallman.ncore
   (:use seesaw.core)
-  (:import java.io.File))
+  (:gen-class)
+  (:import hu.kazocsaba.imageviewer.ImageViewer
+           java.io.File
+           clojure.lang.ISeq
+           javax.imageio.ImageIO))
 
-(def ^{:private true :doc "The user's home folder"} home (System/getProperty "user.home"))
+(defn ^ISeq get-wallpapers [^File dir]
+  "Returns a sequence of all wallpapers in a dir"
+  (-> dir
+      .listFiles
+      seq))
 
-(defn- wallpapers [dir]
-  "Returns a seq of all wallpapers"
-  (when (instance? File dir)
-    (let [files (.listFiles dir)]
-      (zipmap (map #(.getName %) files) files))))
+(defn ^javax.swing.JComponent image-viewer [^File file]
+  "Returns an image viewer component"
+  (-> file
+      ImageIO/read
+      ImageViewer.
+      .getComponent))
 
-(defn make-root-frame [dir]
-  (let [wallpapers (wallpapers dir)
-        lbox (listbox :model (keys wallpapers))
-        preview (label)
-        layout (border-panel :center lbox :east preview)
-        f (frame :content layout :title "Wallpaper Manager" :on-close :exit)]
-    (listen lbox :selection #(config! preview :icon (wallpapers (selection %))))
-    f))
-
-(defn- make-file [x & rst]
-  (loop [a (File. x) b rst]
-    (if (nil? (first b))
-      a
-      (recur (File. a (first b)) (rest b)))))
+(defn wallpaper-chooser [^File dir & {:keys [handler]}]
+  "Returns a combo box containing some wallpapers"
+  (let [combo-box (combobox :model (get-wallpapers dir))]
+    (listen combo-box :selection handler)
+    combo-box))
 
 (defn -main [& args]
-  (-> (make-root-frame (make-file (first args)))
-      pack!
-      show!))
+  "Runs the image viewer.
+  ---------------------------
+  |     |------------|      |
+  |                         |
+  |                         |
+  |                         |
+  |                         |
+  |                         |
+  |                         |
+  |                         |
+  |                         |
+  ---------------------------
+  something like ^"
+  (let [wallpapers (get-wallpapers (File. (first args)))
+        cbox (combobox :model wallpapers :renderer #(config! %1 :text (.getName (:value %2))))
+        applyb (button :text "Apply")
+        vl (vertical-panel :items [cbox "Nada."])
+        f (frame :title "Wallpaper Manager 0.0.2" :content vl)]
+    (listen cbox :selection (fn [e] (config! vl :items [cbox (image-viewer (selection e)) applyb])))
+    (listen applyb :action (fn [e] (.. Runtime getRuntime (exec (str "/usr/bin/feh --bg-scale " (.getPath (selection cbox)))))))
+    (-> f pack! show!)))
